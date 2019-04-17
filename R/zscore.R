1f5d200d56ea15e9093a948180c7a5483c613cc3
dacarras <dacarras@gmail.com>
2019-04-16 21:48:59 -0700

    simple wrapper for z scores

---
+++ R/zscore.R
@@ -0,0 +1,20 @@
+#' zscore() it standardized a continous measure
+#'
+#' @param x a numeric vector
+#'
+#' @return an standardized variable (in z score)
+#' @export
+#'
+#' @examples
+#' \donotrun{
+#' items_data <- items_data %>%
+#'               mutate(zcore = zscore(original))
+#' skimr::skim(items_data$zcore)
+#' }
+# definitive option 
+zscore <- function(x){
+as.numeric(scale(x, center = TRUE, scale = TRUE))
+# Note: simple wrapper, not accounting for
+#       sampling weights
+}   
+

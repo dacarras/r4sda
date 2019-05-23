22c29292549ecf27f15c79afccb704b0e60c829a
dacarras <dacarras@gmail.com>
2019-05-23 11:38:49 -0400

    include senate_weights

---
+++ R/senate_weights.R
@@ -0,0 +1,69 @@
+#' lsa_weights() computes normalized and effective sample size weights for pseudo maximum likelihood mixed models, with large scale assessment data
+#'
+#' @param data data frame object which contains the rest of the arguments
+#' @param wt observations final weight
+#' @param id_k a numeric vector of a single variable, to distinguish between countries
+#' @param scale number to which the weights will be normalized to
+#'
+#' @return the original data frame with the new senate weights at the end, 'ws'
+#' @export
+#'
+#' @details a collection of dplyr::mutate sequences to create normalized and effective sample weights (see Rabe-Hesketh & Skrondal, 2006; Snijder & Bosker, 2012)
+#'
+#' @examples
+#' library(dplyr)
+#' data_frame_with_weights <- data_frame %>%
+#'                            r4sda::lsa_weights(.,
+#'                              scale = 1000,
+#'                              wt = 'wt'
+#'                              id_k = 'id_k')
+#' 
+senate_weights <- function(data, wt, scale, id_k){
+
+# libraries
+require(dplyr)
+
+# weight scale (up to what sum the weights are normalized to)
+k_weight <- scale
+
+
+# create data frame to produce weights
+data_frame  <- data.frame(
+               id_k = as.numeric(data[[id_k]]),
+               wt = as.numeric(data[[wt]])
+               )
+
+require(dplyr)
+data_weights <- data_frame %>%
+                mutate(sum_w = ave(wt, id_k, FUN = sum)) %>%
+                mutate(rscale_f = k_weight/sum_w)             %>%
+                mutate(ws = wt*rscale_f)                      %>%
+                mutate(ws = haven::zap_labels(ws))            %>%
+                dplyr::select(ws) %>%
+                dplyr::tbl_df()
+# add to original data
+data_output <- dplyr::bind_cols(data, data_weights)
+
+# code steps
+
+# How to create a senate weight
+
+# 1. Select the constant to which the sum of 
+#    the weights will be rescaled (in our example, “K”).
+
+# 2. Compute the sum of the sampling weights within each 
+#    of the groups within which the rescaling will be done.
+
+# 3. Multiply the sample weights by the result of dividing 
+#    the constant selected in Step 1 above by the sum of 
+#    the weights in Step 2.
+
+# SOURCE:
+# Gonzalez, E. J. (2012). Rescaling sampling weights and selecting 
+#      mini-samples from large-scale assessment databases. 
+#      IERI Monograph Series Issues and Methodologies in 
+#       Large-Scale Assessments, 5, 115–134.
+
+
+return(data_output)
+}

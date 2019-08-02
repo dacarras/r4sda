820f232d122e09eee32b20fed9448b5d8257c81e
dacarras <dacarras@gmail.com>
2019-08-01 22:15:58 -0400

    updating c_wmean

--- R/c_wmean.R
+++ R/c_wmean.R
@@ -1,39 +1,33 @@
 #' c_wmean() computes the cluster weighted mean for a given variable, and a given clustering variable
 #'
+#' @param data a data frame where the variables are contained
 #' @param x a numeric vector of a single variable
-#' @param wi a numeric vector for within cluster weights
+#' @param w a numeric vector for within cluster weights
+#' @param j a numeric vector for clusters index
 #'
 #' @return a numeric vector
 #' @export
 #'
-#' @details simple wrapper for `ave(variable, group)` function couple with matrixStats::weightedMean(), to estimate cluster means weighted by within cluster weights
+#' @details simple wrapper for  with matrixStats::weightedMean(), to estimate cluster means weighted by within cluster weights
 #'
 #' @examples
 #' library(dplyr)
 #' data_frame %>%
-#' mutate(cluster_mean =  c_wmean(x, w, id_j)) %>%
+#' mutate(cluster_mean =  c_wmean(., 'x', 'w', 'id_j')) %>%
 #' select(x, id_j, cluster_mean) %>%
 #' unique() %>%
 #' print
-c_wmean <- function(x,w,j) {
+# pseudo function
+c_wmean <- function(data, x,w,j) {
 
-  # required library
-  require(dplyr)
-
-  # generate vectores
-  x = as.numeric(x)
-  w = as.numeric(w)
-  j = as.numeric(j)
-
-  # generate temporal data
-  clustered_data <- data.frame(
-    variable = x,
-    weight   = w,
-    cluster  = j
+  data_frame  <- data.frame(
+    x = data[[rlang::quo_name(enquo(x))]],
+    w = data[[rlang::quo_name(enquo(w))]],
+    j = data[[rlang::quo_name(enquo(j))]]
   ) %>%
-    group_by(cluster) %>%
-    mutate(wm = matrixStats::weightedMean(variable,weight, na.rm=TRUE))
+    group_by(j) %>%
+    mutate(wm = matrixStats::weightedMean(x, w, na.rm=TRUE)) %>%
+    ungroup()
 
-  # return
-  return(as.numeric(clustered_data$wm))
+  return(as.numeric(data_frame$wm))
 }

8015955b6989b46bd3e1fafbce6bedeedabad249
dacarras <dacarras@gmail.com>
2019-08-09 18:38:42 -0400

    add mean score

---
+++ R/mean_score.R
@@ -0,0 +1,19 @@
+#' mean_score() computes the mean for a given set of variables in a row wise manner
+#'
+#' @param x,y,z are set of variables from a data frame (i.e. a selection of numeric columns)
+#'
+#' @return a numeric vector
+#' @export
+#'
+#' @details simple wrapper to produce row wise means (default is na.rm=TRUE)
+#'
+#' @examples
+#' library(dplyr)
+#' data_frame %>%
+#' mutate(score =  mean_score(x, z, y))
+#'
+#'
+#'
+mean_score = function(..., na.rm=TRUE){
+rowMeans(cbind(...), na.rm=na.rm)
+}

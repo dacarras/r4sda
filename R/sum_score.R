31c2b8e8c4795230a16ca813571152dcafe0f5a1
dacarras <dacarras@gmail.com>
2019-09-06 16:37:02 -0400

    add sum score

---
+++ R/sum_score.R
@@ -0,0 +1,21 @@
+#' sum_score() computes the mean for a given set of variables in a row wise manner
+#'
+#' @param x,y,z are set of variables from a data frame (i.e. a selection of numeric columns)
+#'
+#' @return a numeric vector
+#' @export
+#'
+#' @details simple wrapper to produce row wise sums (default is na.rm=TRUE)
+#'
+#' @examples
+#' library(dplyr)
+#' data_frame %>%
+#' mutate(score =  sum_score(x, z, y))
+#'
+#'
+#'
+sum_score = function(..., na.rm=TRUE){
+  rowSums(cbind(...), na.rm=na.rm)
+  # source: https://stackoverflow.com/questions/33401788/dplyr-using-mutate-like-rowmeans
+  # by: https://stackoverflow.com/users/1191259/frank
+}

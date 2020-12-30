1750f3364ca3c491727980165d7bc063892381e5
dacarras <dacarras@gmail.com>
2020-12-30 16:03:15 -0300

    wrapper to format number in tables, with a fix number of decimals

---
+++ R/decimal.R
@@ -0,0 +1,20 @@
+#' decimal() takes any numeric variable with decimal places, and returns its aproximation of decimal places to a given number.
+#'
+#' @param x a numeric vector of a single variable
+#'
+#' @return a numeric vector turn into a string
+#' @export
+#'
+#' @details simple wrapper for `format(round(x, k), nsmall=k)` function, to format decimal numbers
+#'
+#' @examples
+#'
+#' number_with_decimals <- .5838384884
+#' number_with_two_decimals <- decimal(number_with_decimals,2)
+#' # result is .58
+#' number_with_two_decimals
+decimal <- function(x, k){
+  format(round(x, k), nsmall=k)
+
+  # Ref: https://stackoverflow.com/questions/3443687/formatting-decimal-places-in-r
+}

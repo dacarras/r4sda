0541dd38348559f8877dd9c864932409d50da9b8
dacarras <dacarras@gmail.com>
2021-02-26 08:33:22 -0300

    update local_path

--- R/local_path.R
+++ R/local_path.R
@@ -1,16 +1,16 @@
-#' local_path() turns a relative directory into a absolute directory. It aids the generation of relative directory to store all code in single folder, and then reproduce the calculations in a Mac OS machine or a Windows machine, or else. Is simple wrapper for `tools::file_path_as_absolute(x)`, to which it adds a final `/`.
+#' local_path() returns the parent directory of the working directory.It aids the generation of relative directory to store all code in single folder, and then reproduce the calculations in a Mac OS machine or a Windows machine, or else.
 #'
-#' @param x a string with directory location
+#' @param x a string with directory location (e.g, '/00_data/', '/01_syntax/', '/02_tables/')
 #'
-#' @return an absolute route or logical directory for the current machine
+#' @return an absolute route or logical directory for the current machine, adding its root directory
 #'
 #' @examples
 #'
 #'
-#' working_directory <- local_path(getwd())
-#' working_directory # this is the current working directory
-#'
+#' parent_directory <- local_path(getwd())
+#' parent_directory #  this is the current working directory
+#'                  #  it assumes the syntax folder, is within the parent directory
 #' @export
 local_path <- function(x){
-paste0(tools::file_path_as_absolute(x),'/')
+  paste0(tools::file_path_as_absolute('..'),x)
 }
